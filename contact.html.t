7bee373e5ecf299efbb9c64918b605f6636e8b9f
elliot <elliot.gorokhovsky@gmail.com>
2017-06-20 14:55:40 +0200

    actual contact fix

--- contact.html.t
+++ contact.html.t
@@ -1,11 +1,10 @@
 <!-- {{templates/header.html}} -->
 <h3> Contact us! </h3>
-<a id="foxyform_embed_link_880145" href="http://www.foxyform.com/"></a>
 <script type="text/javascript">
 (function(d, t){
    var g = d.createElement(t),
        s = d.getElementsByTagName(t)[0];
-   g.src = "http://www.foxyform.com/js.php?id=880145&sec_hash=5f07fdae49b&width=350px";
+   g.src = "https://nanoproductengineering.github.io/website-new/contact.js";
    s.parentNode.insertBefore(g, s);
 }(document, "script"));
 </script>
---
+++ contact.js
@@ -0,0 +1,15 @@
+	(function(d){
+	  var l = d.getElementById("foxyform_embed_link_880145");
+	      i = d.createElement('IFRAME');
+	  i.src = "https://nanoproductengineering.github.io/website-new/contact-iframe.html";
+	  i.width = "350px";
+	  i.height = "380px";
+	  i.frameborder = 0;
+	  i.setAttribute('style', 'border: none;');
+	  i.setAttribute('frameborder', '0');
+	  l.setAttribute('style', 'display: block; width: 350px; font-size: 0.7em; color: grey; text-align: right; text-decoration: none;');
+	  l.parentNode.insertBefore(i,l);
+	})(document);
+
+
+		
